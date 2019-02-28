5a7d468b294f0d9c5b39016353210dfdc9b2f632
Parag <parag@algologic.ai>
2019-02-28 15:59:58 +0100

    adding api code and testing

--- api_sales.py
+++ api_sales.py
@@ -1,6 +1,40 @@
-class ApiSales(object):
-    pass
+from flask import Flask
+from flask_restful import Api, Resource
+from influxdb import InfluxDBClient
+import yaml
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+
+class ApiSales(Resource):
+    def __init__(self):
+        param_file = 'config.yml'
+        with open(param_file, 'r') as ymlfile:
+            self.param_config = yaml.load(ymlfile)
+        self.influxdb_client = InfluxDBClient(self.param_config["INFLUXDB_URL"],
+                                              self.param_config["INFLUXDB_PORT"],
+                                              self.param_config["INFLUXDB_LOGIN"],
+                                              self.param_config["INFLUXDB_PASS"],
+                                              self.param_config["INFLUXDB_DATABASE"]
+                                              )
+
+    def get(self, id):
+        query = "select * from " + self.param_config['MEASUREMENT'] + " where item=\'"+str(id)+"\'"
+        result = self.influxdb_client.query(query)
+        qdata = list(result.get_points())
+        if not qdata:
+            return "Item not found", 404
+        else:
+            return (qdata[0]['quantity']), 200
 
 
 if __name__ == '__main__':
-    pass
+    app = Flask(__name__)
+    api = Api(app)
+    limiter = Limiter(
+        app,
+        key_func=get_remote_address,
+        default_limits=["60 per minute"],
+    )
+    api.add_resource(ApiSales, "/item/<int:id>")
+    app.run(threaded=True)
--- testing.py
+++ testing.py
@@ -1,9 +1,60 @@
 import unittest
+import requests
+import time
+import aiohttp
+import asyncio
+unittest.TestCase.maxDiff = None
 
 
 class ApiTests(unittest.TestCase):
-    pass
+
+    def test_response_200(self):
+        time.sleep(60)
+        urls = ['http://127.0.0.1:5000/item/58',
+                'http://127.0.0.1:5000/item/87'
+                ]
+        responses = []
+        for url in urls:
+            response = requests.get(url).json()
+            responses.append(response)
+        self.assertEqual(responses, [3, 2])
+
+    def test_response_404(self):
+        time.sleep(60)
+        url = 'http://127.0.0.1:5000/item/309999'
+        response = requests.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_max_limit(self):
+        time.sleep(60)
+        url = 'http://127.0.0.1:5000/item/58'
+        responses = []
+        for i in range(0, 61):
+            response = requests.get(url)
+            responses.append(response.status_code)
+        expected_response = [200] * 60 + [429]
+        self.assertEqual(expected_response, responses)
+
+    @staticmethod
+    async def fetch(session, url):
+        async with session.get(url) as response:
+            return await response.json()
+
+    async def req_main(self):
+        time.sleep(60)
+        urls = ['http://127.0.0.1:5000/item/58'] * 60
+        tasks = []
+        async with aiohttp.ClientSession() as session:
+            for url in urls:
+                tasks.append(self.fetch(session, url))
+            responses = await asyncio.gather(*tasks)
+
+        self.assertEqual(responses, [3]*60)
+
+    def test_async_req(self):
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.req_main())
 
 
 if __name__ == '__main__':
-    pass
+    unittest.main()
