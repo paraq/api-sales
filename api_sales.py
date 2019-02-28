4be0ae71848057fad733e4ca67a84cc77b853e5a
Parag <parag@algologic.ai>
2019-02-28 09:23:58 +0100

    adding base

---
+++ api_sales.py
@@ -0,0 +1,6 @@
+class ApiSales(object):
+    pass
+
+
+if __name__ == '__main__':
+    pass
---
+++ testing.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class ApiTests(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    pass
